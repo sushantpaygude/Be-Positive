33f00b6e920e52767b4f9f0c972285beab6c10e9
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-02-02 14:10:40 +0530

    Creating base Fragment & attaching it to MainActivity

--- app/src/main/java/com/example/rnztx/donors/MainActivity.java
+++ app/src/main/java/com/example/rnztx/donors/MainActivity.java
@@ -3,11 +3,33 @@ package com.example.rnztx.donors;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.rnztx.donors.inputForms.BloodRequirement;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if(findViewById(R.id.mainActivity_fragmentContainer)!=null){
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if(savedInstanceState != null)
+                return;
+
+            // create a fragment to be placed in main Activity
+            BloodRequirement mainFragment = new BloodRequirement();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            mainFragment.setArguments(getIntent().getExtras());
+
+            //Add fragment to ActivityMain
+            getSupportFragmentManager().beginTransaction().add(R.id.mainActivity_fragmentContainer,mainFragment).commit();
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/rnztx/donors/inputForms/BloodRequirement.java
@@ -0,0 +1,37 @@
+package com.example.rnztx.donors.inputForms;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rnztx.donors.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BloodRequirement extends Fragment {
+
+
+    public BloodRequirement() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blood_requrement, container, false);
+    }
+
+}
