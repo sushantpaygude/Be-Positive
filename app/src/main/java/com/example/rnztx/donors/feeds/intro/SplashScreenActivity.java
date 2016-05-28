41351fbd4a34116316f53cf98469d7c0fb9aefcd
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-05-28 12:35:55 +0530

    Adding Splashscreen

---
+++ app/src/main/java/com/example/rnztx/donors/feeds/intro/SplashScreenActivity.java
@@ -0,0 +1,55 @@
+package com.example.rnztx.donors.feeds.intro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.rnztx.donors.R;
+import com.example.rnztx.donors.models.auth.SigninActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    private static final int STOPSPLASH = 0;
+    //time duration in millisecond for which your splash screen should visible to
+    //user. here i have taken half second
+    private static final long SPLASHTIME = 5000;
+    TextView bannertxt;
+    //handler for splash screen
+    private Handler splashHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STOPSPLASH:
+                    //Generating and Starting new intent on splash time out
+                    Intent intent = new Intent(getApplicationContext(),
+                            SigninActivity.class);
+                    startActivity(intent);
+                    SplashScreenActivity.this.finish();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        bannertxt=(TextView)findViewById(R.id.banner);
+        bannertxt.setText("Be Positive");
+//        Typeface typeface=Typeface.createFromAsset(getAssets(), "fonts/SimplyRoundedBold.ttf");
+//        bannertxt.setTypeface(typeface);
+        //Generating message and sending it to splash handle
+        Message msg = new Message();
+        msg.what = STOPSPLASH;
+        splashHandler.sendMessageDelayed(msg, SPLASHTIME);
+
+
+    }
+
+}
