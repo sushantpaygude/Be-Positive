5440740e7487a3941bdda1426bb67561878c1987
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-05-29 07:33:43 +0530

    Fixing 9patch / EditText android 5.0 bug

--- app/src/main/java/com/example/rnztx/donors/feeds/intro/auth/SigninActivity.java
+++ app/src/main/java/com/example/rnztx/donors/feeds/intro/auth/SigninActivity.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.rnztx.donors.MainActivity;
 import com.example.rnztx.donors.R;
-import com.example.rnztx.donors.feeds.intro.RegisterActivity;
 import com.example.rnztx.donors.models.UserInfo;
 import com.example.rnztx.donors.models.utils.Constants;
 import com.example.rnztx.donors.models.utils.Utilities;
@@ -121,8 +121,15 @@ public class SigninActivity extends AppCompatActivity implements GoogleApiClient
             storeUsersInfo();
 
             // start Main Activity
-//            Intent intent = new Intent(this, MainActivity.class);
-            Intent intent = new Intent(this, RegisterActivity.class);
+            UserInfo userInfo = Utilities.userInfoMap.get(Utilities.getUserId());
+
+            Intent intent;
+//            if (!userInfo.getMobileNumber().equals(Constants.PREF_DEFAULT_VALUE)){
+//                intent = new Intent(this, MainActivity.class);
+//            }
+//            else
+//                intent = new Intent(this, RegisterActivity.class);
+            intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         } else {
             // Signed out, show unauthenticated UI.
@@ -138,7 +145,7 @@ public class SigninActivity extends AppCompatActivity implements GoogleApiClient
                 Utilities.getUserEmail(),
                 Utilities.getUserDisplayName(),
                 Utilities.getUserPhotoUrl());
-
+        
         fUsersLocation
                 .child(Utilities.getUserId())
                 .setValue(currentUser, new Firebase.CompletionListener() {
